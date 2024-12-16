93ed7cd65ab0278fbe3b80c98f4f25bbd53278af
sedun denis <deonisij-tyrov@mail.ru>
2024-12-17 00:20:58 +0300

    getByIdWithStepVerifier

--- src/test/java/guru/springframework/reactiveexamples/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/reactiveexamples/PersonRepositoryImplTest.java
@@ -4,6 +4,7 @@ import guru.springframework.reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +19,14 @@ class PersonRepositoryImplTest {
         assertEquals(1, block.getId());
         assertEquals(block.getFirstName(), "firstname1");
     }
+
+    @Test
+    void getByIdWithStepVerifier() {
+        StepVerifier.create(personRepository.getById(1)).expectNextCount(1).verifyComplete();
+
+        StepVerifier.create(personRepository.getById(100)).expectNextCount(0).verifyComplete();
+    }
+
     @Test
     void getByIdIfNotExists() {
         Mono<Person> byId = personRepository.getById(100);
